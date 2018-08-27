1031921e9d1f0bbb26291ec3391b040768af668a
Asmita Khadka <10015309@student.toiohomai.ac.nz>
2018-08-27 15:13:45 +1200

    switch statement

---
+++ Question 1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter pet name");
+            string pet = Console.ReadLine();
+            switch (pet)
+            {
+                case "dog":
+                    Console.WriteLine("yes");
+                    break;
+                case "cow":
+                    Console.WriteLine("yes");
+                    break;
+
+                case "rat":
+                    Console.WriteLine("no");
+                    break;
+                default:
+                    Console.WriteLine("no idea about animal");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
